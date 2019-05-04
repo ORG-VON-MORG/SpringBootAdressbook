9da2f742e4ceb0d977e27a80a7ca79d6219acc67
master <orggottschalk@web.de>
2019-05-04 15:58:08 +0200

    passing data to jquery

---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,12 @@
+function f(firstname, lastname, street, city, postalCode, birthday){
+
+    $("#firstname").html(firstname);
+    $("#lastname").html(lastname);
+    $("#street").html(street);
+    $("#city").html(city);
+    $("#postalCode").html(postalCode);
+    $("#birthday").html(birthday);
+
+
+
+}
